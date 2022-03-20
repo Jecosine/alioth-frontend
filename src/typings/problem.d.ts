0bc59207addd6754643e6234dc1c1abbb89f769b
jecosine <jecosine@gmail.com>
2022-03-20 17:59:40 +0800

    feat(service): Add problems typings

---
+++ src/typings/problem.d.ts
@@ -0,0 +1,14 @@
+declare interface ProblemTagMinimal {
+  id: number;
+  name: string;
+}
+
+declare interface ProblemMinimal {
+  id: number;
+  title: string;
+  difficulty: string; // TODO: customize difficulty settings
+  attempt: number;
+  solved: number;
+  author: UserDataMinimal;
+  tags: Array<ProblemTagMinimal>;
+}
